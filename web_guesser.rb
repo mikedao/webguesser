456999c3dd499ceb1a761b754ede99b71a09f033
Michael Dao <michael.dao@gmail.com>
2014-12-15 14:27:06 -0700

    Finished Logic

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,33 @@
 require 'sinatra'
-
+require 'sinatra/reloader'
 
 	secret_number = rand(101)
 
 get '/' do
-	"The secret number is #{secret_number}."
+	user_guess 	= params["guess"].to_i
+	message 		= ""
+	message 		= feedback(user_guess, secret_number) if user_guess != 0
+
+	erb :index, locals: { :secret_number 	=> secret_number,
+												:message 				=> message
+											}
+end
+
+
+def feedback(user_guess, secret_number)
+	if user_guess > secret_number
+		if user_guess - secret_number > 10
+			"Way too high."
+		else
+			"Too high."
+		end
+	elsif user_guess < secret_number
+		if secret_number - user_guess > 10
+			"Way too low."
+		else
+			"Too low."
+		end
+	else
+		"Correct! The secret number is #{secret_number}"
+	end
 end
